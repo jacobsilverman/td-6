af1cc06d5779c6e4b74d8d3e21c0f1135e3f8df8
js1 <jacob@slvr.mn>
2019-01-22 18:48:35 +1100

    todo: remove e.origin feature

--- logger.js
+++ logger.js
@@ -1,5 +1,18 @@
+const fs = require('fs');
+var moment = require('moment');
+var zone = require('moment-timezone');
 
-function write(message){
-  console.log('logger ', message)
+function write(error){
+  console.log('error ',error)
+  let stream = fs.createWriteStream("log.txt", {flags:'a'});
+  // let  = moment().format("ddd MMM DD YYYY HH:mm:ss ");
+  let stamp = moment().toString() + ' ' + zone.tz(zone.tz.guess()).zoneAbbr();
+  if (error.origin){
+    stream.write(stamp + ' Problem ' + error.origin + ' ' + error.e + "\n");
+    stream.end();
+  } else {
+    stream.write(stamp + ' ' + error.e + "\n");
+    stream.end();
+  }
 }
 module.exports = { write };
--- scraper.js
+++ scraper.js
@@ -1,36 +1,8 @@
-/*
-  PROBLEM 1: Program your scraper to check for a folder called ‘data’. If the folder doesn’t exist, the scraper
-   should create one. If the folder does exist, the scraper should do nothing. SOLUTION 1: Use the Console and File
-    System Node.js APIs to check if the directory and file for today exist. If they do not then create them.
- */
-
-/*
-  PROBLEM 2: Your scraper should visits the website http://shirts4mike.com and use http://shirts4mike.com/shirts.php
-   as single entry point to scrape information for 8 tee-shirts from the site, without using any hard-coded urls
-    like http://www.shirts4mike.com/shirt.php?id=101. If you’re unsure of how to get started, try googling ‘node
-     scraper’ to get a feel for what a scraper is and what it does. SOLUTION 2: For scraping content from the site,
-      either use a scraping module or use the Cheerio module to create your own scraper. To create the CSV file, use
-       a CSV creation module. I chose json2csv because it has At least 1,000 downloads and has been updated in the last
-        six months.
- */
-
-/* http://shirts4mike.com is served via HTTP (not HTTPS) so I decided to use HTTP Node.js API */
-
-/* use system events such as Data Events, Completion Events, Error Events */
-
-/*
-httpRequest.onreadystatechange = () => {
-  switch(httpRequest.readyState){
-    case XMLHttpsRequest.DONE:
-      console.log("Finished");
-      break;
-  }
-}
-  */
-
+/* import npm  libraries */
 var fetch = require('node-fetch');
 var cheerio = require('cheerio');
 var moment = require('moment');
+/* import custom functions */
 var writer = require('./writer');
 var logger = require('./logger');
 
@@ -41,11 +13,16 @@ let scrape_urls = [];
 let final_set = [];
 
 function handleErrors(response) {
-  if (!response.ok) {
-    console.log('throwing error')
-    throw Error(response);
-  }
-  return response;
+
+    if (response.ok) {
+      resolve(response);
+    } else if (response.status > 300) {
+      reject({e: response.status, origin: 'Some server error'})
+    } else if (response.status < 200){
+      reject({e: response.status, origin: 'Unauthorized'})
+    } else {
+      reject({e: response.status, origin: 'default'})
+    }
 }
 
 function fetch1(){
@@ -61,12 +38,10 @@ function fetch1(){
           $('.products a').each(function () {
             scrape_urls.push($(this).attr('href'));
           });
-          console.log('fetch1 resolve()');
-          throw Error('responses')
           resolve();
         } catch(e){
-          console.log('fetch1 reject()', e);
-          reject(e);
+          const origin = 'fetching urls from homepage';
+          reject({ e, origin });
         }
       });
     })
@@ -94,11 +69,10 @@ async function fetch2(){
             final_set.push(item_obj);
           })
       }
-      console.log('fetch2 resolve()');
       resolve();
     } catch(e){
-      console.log('fetch2 reject()', e);
-      reject(e);
+      const origin = 'scraping data from t-shirt pages';
+      reject({ e, origin });
     }
   });
 }
@@ -107,30 +81,24 @@ function write_file(){
   writer.write(final_set)
 }
 
-function write_error(error,origin){
-  /* check if the error is from the fetch or from a promise reject(e) */
-  if(error.statusText){
-    console.log('statusText ', error.statusText);
-  }
-  // console.log('error', error)
-  logger.write(error)
-}
-
 async function main(){
   /* */
   try {
     await fetch1()
-  } catch(e){
-    write_error(e,'fetch1');
+  } catch(error){
+    console.log('catching f1: ', error.origin)
+    logger.write(error)
   }
   /* */
   try {
     await fetch2()
-  } catch(e){
-    write_error(e, 'fetch2');
+  } catch(error){
+    console.log('catching f2: ', error)
+    logger.write(error)
   }
   /* */
   write_file()
 }
+
 main();
 
